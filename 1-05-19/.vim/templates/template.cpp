4a53325982be5caa1504a7e43e3dc5414cc40caf
kpan2034 <keyur.panchal@gmail.com>
2019-11-09 20:06:40 +0530

    Renamed and reorganized using jbernard/dotfiles

---
+++ 1-05-19/.vim/templates/template.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+typedef vector<pii> vp;
+
+#define forn(i,a,n) for(ll i=a; i<n; i++)
+#define forr(i,n,a) for(ll i=n; i>a; i--)
+#define pb push_back
+#define mp make_pair
+#define Inf 2000000010
+#define INF LONG_LONG_MAX
+#define mod 10e7
+
+
+int main(){
+	return 0;
+}
+
---
+++ 11-11-19/vim/templates/template.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#define debug
+
+#define lp(X) for(ll i=0; i<X; i++)
+#define lpi(X) for(ll i=0; i<=X; i++)
+#define lpd(X) for(ll i=X-1; i>=0; i--)
+#define lpdi(X) for(ll i=X; i>=0; i--)
+#define all(X) int i=X.begin();i<=X.end();i++;
+#define iter(X) for(auto it : X)
+#define pb push_back
+#define mp make_pair
+#define vll vector<ll>
+#define vi vector<int>
+#define pii pair<int, int> 
+#define pll pair<ll, ll>
+#define prq priority_queue<ll>
+#define vpll vector<pll>
+#define vp vector<pii>
+#define GET_MACRO(_1, _2, _3, NAME, ...) NAME
+#define in1(X) cin >> X;
+#define in2(X, Y) cin >> X >> Y;
+#define in3(X, Y, Z) cin >> X >> Y >> Z;
+#define in(...) GET_MACRO(__VA_ARGS__, in3, in2, in1)(__VA_ARGS__)
+#define fzf1(X) cout << X << endl;
+#define fzf2(X, Y) cout << X << "****" << Y << endl;
+#define fzf3(X, Y, Z) cout << X << "****" <<  Y << "****" << Z << endl;
+#define getEdge(G, U, V) in2(U, V);--U;--V;G[U].pb(V);G[V].pb(U);
+#ifdef debug
+#define fzf(...) GET_MACRO(__VA_ARGS__, fzf3, fzf2, fzf1)(__VA_ARGS__)
+#else
+#define fzf(...) /**/
+#endif
+
+const int N = int(1e9+7);
+const int mod = int(1e9+7);
+const double eps = double(1e-9);
+
+using namespace std;
+typedef long long ll;
+
+ll n, k;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    {{CURSOR}}
+	return 0;
+}
+
